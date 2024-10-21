8bb249a796064370f631b84cb6ea006b8168cafb
Seri Park <parkseridev@gmail.com>
2024-10-22 00:30:21 +0900

    feat: cartUtil.calculateItemTotal 내부 로직 작성

--- src/refactoring/hooks/utils/cartUtils.ts
+++ src/refactoring/hooks/utils/cartUtils.ts
@@ -1,21 +1,33 @@
-import { CartItem, Coupon } from "../../../types";
+import { CartItem, Coupon } from '../../../types';
 
 export const calculateItemTotal = (item: CartItem) => {
-  return 0;
+  const rate =
+    item.product.discounts.find(
+      (discount) => discount.quantity === item.quantity
+    )?.rate ?? 0;
+
+  return item.product.price * item.quantity * (1 - rate);
 };
 
 export const getMaxApplicableDiscount = (item: CartItem) => {
   return 0;
 };
 
-export const calculateCartTotal = (cart: CartItem[], selectedCoupon: Coupon | null) => {
+export const calculateCartTotal = (
+  cart: CartItem[],
+  selectedCoupon: Coupon | null
+) => {
   return {
     totalBeforeDiscount: 0,
     totalAfterDiscount: 0,
-    totalDiscount: 0,
+    totalDiscount: 0
   };
 };
 
-export const updateCartItemQuantity = (cart: CartItem[], productId: string, newQuantity: number): CartItem[] => {
-  return []
+export const updateCartItemQuantity = (
+  cart: CartItem[],
+  productId: string,
+  newQuantity: number
+): CartItem[] => {
+  return [];
 };
